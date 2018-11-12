f6b16a7e0f70d8488c8296f2f8d6aa5829291cb3
ranggakusuma <ranggavskusuma@gmail.com>
2018-11-12 19:40:55 +0700

    add crud 3 tables

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -1,23 +1,23 @@
-const Voters = require('../models/Voters')
+const Model = require('../models/Model')
 const View = require('../views/View')
 
 class Controller {
     static insert (param) {
-        Voters.insert(param, function(err) {
+        Model.insert(param, function(err) {
             if (err) {
                 View.displayErr({
-                    message: 'error insert data table voters',
+                    message: 'error insert data table',
                     err: err
                 })
             } else {
-                View.displayMsg(`Berhasil insert data ke table voters`)
+                View.displayMsg(`Berhasil insert data ke table`)
             }
         })
 
     }
 
     static update (param) {
-        Voters.update(param, function(err) {
+        Model.update(param, function(err) {
             if (err) {
                 View.displayErr({
                     message: 'error update data table voters',
@@ -30,14 +30,14 @@ class Controller {
     }
 
     static delete(param) {
-        Voters.delete(param, function(err){
+        Model.delete(param, function(err){
             if (err) {
                 View.displayErr({
                     message: 'error delete data table voters',
                     err: err
                 })
             } else {
-                View.displayMsg(`Berhasil delete data id ke ${param[0]}`)
+                View.displayMsg(`Berhasil delete data id ke ${param[1]} table ${param[0]}`)
             }
         })
     }
@@ -45,9 +45,9 @@ class Controller {
     static help () {
         View.displayMsg(`
         list command:
-        1. insert <first_name> <last_name> <gender> <age>
-        2. update <id> <first_name> <last_name> <gender> <age>
-        3. delete <id>
+        1. insert <table name> <parameter>
+        2. update <table name> <parameter>
+        3. delete <table name> <id>
         `)
     }
 }
